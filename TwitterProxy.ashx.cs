e72042524228b8a66cbd3e3905e5da26b89eefa7
Dave Ward <dave@encosia.com>
2011-08-01 09:44:28 -0400

    Simplified for the basic example

--- TwitterProxy.ashx.cs
+++ TwitterProxy.ashx.cs
@@ -10,35 +10,29 @@ namespace HttpHandler_Proxy {
     public void ProcessRequest(HttpContext context) {
       string screenName = context.Request.QueryString["screenName"];
 
-      if (string.IsNullOrWhiteSpace(screenName)) {
+      if (string.IsNullOrWhiteSpace(screenName))
         screenName = "Encosia";
-      }
 
-      bool shouldCache = bool.Parse(context.Request.QueryString["shouldCache"]);
+      string cachedResponse = context.Cache.Get("twitter-status-" + screenName) as string;
 
-      if (shouldCache) {
-        string cachedResponse = context.Cache.Get("twitter-status-" + screenName).ToString();
+      if (!string.IsNullOrWhiteSpace(cachedResponse)) {
+        context.Response.ContentType = "application/json";
+        context.Response.Write(cachedResponse);
 
-        if (!string.IsNullOrWhiteSpace(cachedResponse)) {
-          context.Response.ContentType = "application/json";
-          context.Response.Write(cachedResponse);
-        }
+        return;
       }
 
       WebClient twitter = new WebClient();
 
-      //string requestUrl = "http://api.twitter.com/1/" + HttpUtility.UrlDecode(context.Request.QueryString.ToString());
-      string requestUrl = "http://api.twitter.com/1/statuses/user_timeline.json?screenName=" + screenName;
+      string requestUrl = "http://api.twitter.com/1/statuses/user_timeline.json?id=" + screenName;
 
       string response = twitter.DownloadString(requestUrl);
 
-      if (shouldCache) {
-        // This monstrosity essentially just caches the result for twitter-status-screenName,
-        //  with a maximum lifetime of 5 minutes from the time that we first cached it.
-        context.Cache.Add("twitter-status-" + screenName, response, 
-          null, DateTime.Now.AddMinutes(5), System.Web.Caching.Cache.NoSlidingExpiration, 
-          System.Web.Caching.CacheItemPriority.Normal, null);
-      }
+      // This monstrosity essentially just caches the result for twitter-status-screenName,
+      //  with a maximum lifetime of 5 minutes from the time that we first cached it.
+      context.Cache.Add("twitter-status-" + screenName, response,
+        null, DateTime.Now.AddMinutes(5), System.Web.Caching.Cache.NoSlidingExpiration,
+        System.Web.Caching.CacheItemPriority.Normal, null);
 
       context.Response.ContentType = twitter.ResponseHeaders["content-type"];
       context.Response.Write(response);
