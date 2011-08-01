3805fb4887f7bb18332c4edd5605cafd41347ba0
Dave Ward <dave@encosia.com>
2011-08-01 19:30:11 -0400

    Updated to better match the post's code snippets

--- TwitterProxy.ashx.cs
+++ TwitterProxy.ashx.cs
@@ -8,34 +8,42 @@ namespace HttpHandler_Proxy {
   public class TwitterProxy : IHttpHandler {
 
     public void ProcessRequest(HttpContext context) {
-      string screenName = context.Request.QueryString["screenName"];
+      // This will be the case whether there's a cache hit or not.
+      context.Response.ContentType = "application/json";
 
-      if (string.IsNullOrWhiteSpace(screenName))
-        screenName = "Encosia";
+      string id = context.Request.QueryString["id"];
 
-      string cachedResponse = context.Cache.Get("twitter-status-" + screenName) as string;
+      if (string.IsNullOrWhiteSpace(id))
+        id = "Encosia";
 
-      if (!string.IsNullOrWhiteSpace(cachedResponse)) {
-        context.Response.ContentType = "application/json";
-        context.Response.Write(cachedResponse);
+      // Check to see if the twitter status is already cached,
+      //   then retrieve and return the cached value if so.
+      if (context.Cache["tweets-" + id] != null) {
+        string cachedTweets = context.Cache["tweets-" + id].ToString();
 
+        context.Response.Write(cachedTweets);
+
+        // We're done here.
         return;
       }
 
       WebClient twitter = new WebClient();
 
-      string requestUrl = "http://api.twitter.com/1/statuses/user_timeline.json?id=" + screenName;
+      string requestUrl = "http://api.twitter.com/1/statuses/user_timeline.json?id=" + id;
 
-      string response = twitter.DownloadString(requestUrl);
+      string tweets = twitter.DownloadString(requestUrl);
 
-      // This monstrosity essentially just caches the result for twitter-status-screenName,
-      //  with a maximum lifetime of 5 minutes from the time that we first cached it.
-      context.Cache.Add("twitter-status-" + screenName, response,
-        null, DateTime.Now.AddMinutes(5), System.Web.Caching.Cache.NoSlidingExpiration,
-        System.Web.Caching.CacheItemPriority.Normal, null);
+      // This monstrosity essentially just caches the WebClient result
+      //  with a maximum lifetime of 5 minutes from now.
+      // If you don't care about the expiration, this can be a simple
+      //  context.Cache["tweets"] = tweets; instead.
+      context.Cache.Add("tweets-" + id, tweets,
+        null, DateTime.Now.AddMinutes(5),
+        System.Web.Caching.Cache.NoSlidingExpiration,
+        System.Web.Caching.CacheItemPriority.Normal, 
+        null);
 
-      context.Response.ContentType = twitter.ResponseHeaders["content-type"];
-      context.Response.Write(response);
+      context.Response.Write(tweets);
     }
 
     public bool IsReusable {
